1fd4898e216af074ff6187aba64cca0d65726723
Matheus Consoli <typelevelconsoli@gmail.com>
2025-02-28 10:38:46 -0300

    improve structure

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
+use std::collections::BinaryHeap;
 use std::env;
+use std::fmt::Display;
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
 use tree_sitter::StreamingIterator;
 use tree_sitter::{Parser, Query, QueryCursor};
 use walkdir::WalkDir;
-
-fn parse_args() -> (String, usize) {
-    // Parse command line arguments
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: {} <directory> <min_args>", args[0]);
-        std::process::exit(1);
-    }
-
-    let directory = args[1].clone();
-    let min_args = args[2]
-        .parse::<usize>()
-        .expect("Minimum arguments must be a number");
-
-    (directory, min_args)
-}
 
 const QUERY_SOURCE: &str = r#"
     (function_item
@@ -32,27 +18,34 @@ const QUERY_SOURCE: &str = r#"
       parameters: (parameters) @params)
     "#;
 
+fn parse_args() -> (PathBuf, usize) {
+    // Parse command line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} <directory> <min_args>", args[0]);
+        std::process::exit(1);
+    }
+
+    let directory = PathBuf::from(&args[1]);
+    let min_args = args[2]
+        .parse::<usize>()
+        .expect("Minimum arguments must be a number");
+
+    (directory, min_args)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (directory, min_args) = parse_args();
 
-    // Initialize tree-sitter
     let mut parser = Parser::new();
     parser.set_language(&tree_sitter_rust::LANGUAGE.into())?;
 
-    // Create a Tree-sitter query to find functions
-    // The query captures function signature, name, and parameters list
-
     let query = Query::new(&tree_sitter_rust::LANGUAGE.into(), QUERY_SOURCE)?;
 
-    // Iterate through Rust files in the directory
-    let mut total_count = 0;
-    println!(
-        "Searching for functions with more than {} arguments in {}",
-        min_args, directory
-    );
-    println!("--------------------------------------------------");
+    let mut total_files = 0;
 
-    let directory = PathBuf::from(directory);
+    let mut bucket = BinaryHeap::new();
+
     for entry in WalkDir::new(&directory)
         .follow_links(true)
         .into_iter()
@@ -60,17 +53,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .filter(|e| e.path().extension().map_or(false, |ext| ext == "rs"))
     {
         let path = entry.path();
-        let file_count = process_file(&directory, &path, &mut parser, &query, min_args)?;
-        total_count += file_count;
+        let file_count = process_file(
+            &directory,
+            &path,
+            &mut parser,
+            &query,
+            min_args,
+            &mut bucket,
+        )?;
+        total_files += file_count;
     }
 
-    println!("--------------------------------------------------");
-    println!(
-        "Found {} functions with more than {} arguments",
-        total_count, min_args
-    );
+    for el in bucket {
+        println!("{el}");
+    }
+
+    println!("\nFound {total_files} functions with more than {min_args} arguments");
 
     Ok(())
+}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+struct FnInfo {
+    path: PathBuf,
+    name: String,
+    arity: usize,
+    line: usize,
+}
+
+impl PartialOrd for FnInfo {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.arity.partial_cmp(&other.arity)
+    }
+}
+impl Ord for FnInfo {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.arity.cmp(&other.arity)
+    }
+}
+
+impl Display for FnInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}:{}: fn {}/{}",
+            self.path.display(),
+            self.line,
+            self.name,
+            self.arity
+        )
+    }
 }
 
 fn process_file(
@@ -79,6 +111,7 @@ fn process_file(
     parser: &mut Parser,
     query: &Query,
     min_args: usize,
+    bucket: &mut BinaryHeap<FnInfo>,
 ) -> Result<usize, Box<dyn std::error::Error>> {
     let source_code = fs::read_to_string(path)?;
     let tree = parser
@@ -89,36 +122,30 @@ fn process_file(
     let mut matches = cursor.matches(query, tree.root_node(), source_code.as_bytes());
 
     let function_name_idx = query.capture_index_for_name("function_name").unwrap_or(0);
-    let method_name_idx = query.capture_index_for_name("method_name").unwrap_or(0);
     let params_idx = query.capture_index_for_name("params").unwrap_or(0);
 
     let mut count = 0;
 
     while let Some(m) = matches.next() {
-        // Get function/method name
-        let name_capture = m
-            .captures
-            .iter()
-            .find(|c| c.index == function_name_idx || c.index == method_name_idx);
+        let name_capture = m.captures.iter().find(|c| c.index == function_name_idx);
 
         let params_capture = m.captures.iter().find(|c| c.index == params_idx);
 
         if let (Some(name), Some(params)) = (name_capture, params_capture) {
-            let name_str = &source_code[name.node.byte_range()];
+            let name = source_code[name.node.byte_range()].to_owned();
             let params_node = params.node;
 
-            let arg_count = count_parameters(&params_node);
+            let arity = count_parameters(&params_node);
 
-            let path = path.strip_prefix(base).unwrap();
-            if arg_count > min_args {
+            let path = path.strip_prefix(base).unwrap().to_path_buf();
+            if arity > min_args {
                 let line = params_node.start_position().row + 1;
-                println!(
-                    "{} args\t{:4}| fn {}\t{}",
-                    arg_count,
+                bucket.push(FnInfo {
+                    path,
+                    name,
+                    arity,
                     line,
-                    name_str,
-                    path.display(),
-                );
+                });
                 count += 1;
             }
         }
